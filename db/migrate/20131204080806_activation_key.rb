d467f861d0ba3d32ccdc47100f77b283b966e7ac
Joseph Magen <joseph@isratrade.co.il>
2013-12-04 10:07:39 +0200

    copy engine.rb and routes/mount_engine

---
+++ db/migrate/20131204080806_activation_key.rb
@@ -0,0 +1,7 @@
+class ActivationKey < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- lib/katello/engine.rb
+++ lib/katello/engine.rb
@@ -1,5 +1,18 @@
 module Katello
+
   class Engine < ::Rails::Engine
+
+    initializer 'katello.mount_engine', :after=> :build_middleware_stack do |app|
+      app.routes_reloader.paths << "#{Katello::Engine.root}/config/routes/mount_engine.rb"
+    end
+
     isolate_namespace Katello
+
+    initializer "katello.load_app_instance_data" do |app|
+      app.config.paths['db/migrate'] += Katello::Engine.paths['db/migrate'].existent
+      app.config.autoload_paths += Dir["#{config.root}/app/lib)"]
+    end
+
   end
+
 end
